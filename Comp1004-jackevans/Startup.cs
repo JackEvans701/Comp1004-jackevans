f5c24dcc350e4368814afd792bbdac59a0d5572b
jackevans3 <jack-evans.37@students.plymouth.ac.uk>
2023-03-16 08:54:54 +0000

    Add project files.

---
+++ Comp1004-jackevans/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Comp1004_jackevans
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+            app.UseRouting();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ Comp1004-jackevans/UploadController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace Comp1004_jackevans
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index(IFormFile files)
+        {
+            var fileName = Path.GetFileName(files.FileName);
+            var uniqueFileName = Convert.ToString(Guid.NewGuid());
+            var fileExtension = Path.GetExtension(fileName);
+            var newFileName = String.Concat(uniqueFileName, fileExtension);
+            var filePath = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newFileName}";
+            using (FileStream fs = System.IO.File.Create(filePath))
+            {
+                files.CopyTo(fs);
+                fs.Flush();
+            }
+            return View();
+        }
+    }
+}
